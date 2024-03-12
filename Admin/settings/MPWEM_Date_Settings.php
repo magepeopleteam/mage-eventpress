77d920bbdcdd5de9a2049787309bc00dba96aed2
sumonazma <engr.sumonazma@gmail.com>
2024-03-12 10:59:24 +0600

    start event date tab

---
+++ Admin/settings/MPWEM_Date_Settings.php
@@ -0,0 +1,16 @@
+<?php
+	/*
+* @Author 		engr.sumonazma@gmail.com
+* Copyright: 	mage-people.com
+*/
+	if (!defined('ABSPATH')) {
+		die;
+	} // Cannot access pages directly.
+	if (!class_exists('MPWEM_Date_Settings')) {
+		class MPWEM_Date_Settings {
+			public function __construct() {
+
+			}
+		}
+		new MPWEM_Date_Settings();
+	}
---
+++ Admin/settings/MPWEM_Settings.php
@@ -0,0 +1,12 @@
+<?php
+	/*
+* @Author 		engr.sumonazma@gmail.com
+* Copyright: 	mage-people.com
+*/
+	if (!defined('ABSPATH')) {
+		die;
+	} // Cannot access pages directly.
+	if (!class_exists('MPWEM_Settings')) {
+		class MPWEM_Settings {
+		}
+	}

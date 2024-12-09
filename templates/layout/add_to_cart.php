8aea0f05eb7035724fc56162bca18e30974bf62f
sumonazma <engr.sumonazma@gmail.com>
2024-12-09 12:09:11 +0600

    backend _order key problem solve

--- templates/layout/add_to_cart.php
+++ templates/layout/add_to_cart.php
@@ -7,13 +7,13 @@
 		die;
 	} // Cannot access pages directly.
 	$event_id = $event_id ?? 0;
-	$backend_order = MP_Global_Function::data_sanitize($_POST['backend_order']);
+	//$backend_order = MP_Global_Function::data_sanitize($_POST['backend_order']);
 	$link_wc_product = MP_Global_Function::get_post_info($event_id, 'link_wc_product');;
 ?>
 	<div class="col_12 mpwem_form_submit_area mT_xs">
 		<div class="justifyBetween _alignCenter">
 			<h5 class="_mpBtn"><?php esc_html_e('Total Price : ', 'mage-eventpress'); ?><span class="mpwem_total _textTheme"><?php echo wc_price(0); ?></span></h5>
-			<?php if ($backend_order>0) { ?>
+			<?php if ( is_admin() && str_contains( wp_get_referer(), 'admin_purchase_ticket' ) ) { ?>
 				<button type="submit" class="_themeButton">
 					<?php esc_html_e('Book Now ', 'mage-eventpress'); ?>
 				</button>

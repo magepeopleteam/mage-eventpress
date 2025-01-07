db1fc81c6a04ae71e36f6e2eb2e543257b55557d
Shahadat <raselsha@gmail.com>
2025-01-07 13:25:32 +0600

    fix: related items

--- Admin/settings/MPWEM_Related_Settings.php
+++ Admin/settings/MPWEM_Related_Settings.php
@@ -8,7 +8,7 @@
 				add_action( 'woocommerce_product_options_related', [$this,'woocom_linked_products_data_custom_field'] );
 				add_action( 'woocommerce_process_product_meta', [$this,'woocom_linked_products_data_custom_field_save'] );
 				add_action( 'woocommerce_after_single_product', [$this,'related_single_products'] );
-				add_action( 'mpwem_related', [$this,'related_events_after_single'] );
+				add_action( 'after-single-events', [$this,'related_events'] );
 
 				add_action('mep_admin_event_details_before_tab_name_rich_text',[$this,'event_related_tab']);
 				add_action('mp_event_all_in_tab_item',[$this,'event_related_content']);
@@ -173,34 +173,175 @@
 				}
 		
 				$style = 'grid'; ?>
-				<div class="mep_event_list related-event-products">
+				<div class="section-heading">
 					<h2><?php echo $section_label; ?></h2>
-					<div class="mep_event_list_sec">
-						<?php
-							$event_expire_on 			= mep_get_option( 'mep_event_expire_on_datetime', 'general_setting_sec', 'event_start_datetime');
-							$now                        = current_time('Y-m-d H:i:s');
-							$args_search_qqq = array (
-								 'post_type'            => array( 'mep_events' ),
-								 'posts_per_page'       => -1,
-								 'post__in'             => $product_ids,
-								 'order'                => 'ASC',
-								 'orderby'              => 'meta_value',
-								 'meta_key'             => 'event_start_datetime',
-								 'meta_query'           => array(
-									array(
-											'key'       => $event_expire_on,
-											'value'     => $now,
-											'compare'   => '>'
-										)
+				</div>
+				<div class="related_events">
+					<?php
+						$event_expire_on 			= mep_get_option( 'mep_event_expire_on_datetime', 'general_setting_sec', 'event_start_datetime');
+						$now                        = current_time('Y-m-d H:i:s');
+						$args_search_qqq = array (
+								'post_type'            => array( 'mep_events' ),
+								'posts_per_page'       => -1,
+								'post__in'             => $product_ids,
+								'order'                => 'ASC',
+								'orderby'              => 'meta_value',
+								'meta_key'             => 'event_start_datetime',
+								'meta_query'           => array(
+								array(
+										'key'       => $event_expire_on,
+										'value'     => $now,
+										'compare'   => '>'
 									)
-			
-							);
-							$loop = new WP_Query( $args_search_qqq );
+								)
 		
-						if ( is_array( $product_ids ) && sizeof($product_ids) > 0 ) {
-							while ($loop->have_posts()) {
-							$loop->the_post(); 
-							$values = get_the_id();
+						);
+						$loop = new WP_Query( $args_search_qqq );
+	
+					if ( is_array( $product_ids ) && sizeof($product_ids) > 0 ) {
+						while ($loop->have_posts()) {
+						$loop->the_post(); 
+						$values = get_the_id();
+							$event_meta         = get_post_custom( $values );
+							$tt                 = get_the_terms( $values, 'mep_cat' );
+							$org_class          = mep_get_term_as_class( $values, 'mep_org' );
+							$torg               = get_the_terms( $values, 'mep_org' );
+							$cat_class          = mep_get_term_as_class( $values, 'mep_cat' );
+							$available_seat     = mep_get_total_available_seat( $values, $event_meta );
+							$show_price         = mep_get_option( 'mep_event_price_show', 'general_setting_sec', 'yes' );
+							$show_price_label   = mep_get_option( 'event-price-label', 'general_setting_sec', 'Price Starts from:' );
+							$author_terms       = get_the_terms( $values, 'mep_org' );
+							?>
+							<div class='related_items <?php echo $columnNumber . ' '; ?>mep_event_<?php echo $style; ?>_item mix <?php if ( $tt ) {
+								echo 'mage-' . $org_class;
+							} ?> <?php if ( $torg ) {
+								echo 'mage-' . $cat_class;
+							} ?>'>
+								<div class="mep_list_thumb">
+									<a href="<?php echo get_the_permalink( $values ); ?>"><?php echo get_the_post_thumbnail( $values ); ?></a>
+									<div class="mep-ev-start-date">
+										<div class="mep-day"><?php echo date_i18n( 'd', strtotime( $event_meta['mep_event_start_date'][0] ) ); ?></div>
+										<div class="mep-month"><?php echo date_i18n( 'M', strtotime( $event_meta['mep_event_start_date'][0] ) ); ?></div>
+									</div>
+								</div>
+								<div class="mep_list_event_details"><a
+											href="<?php echo get_the_permalink( $values ); ?>">
+										<div class="mep-list-header">
+											<h2 class='mep_list_title'><?php echo get_the_title( $values ); ?></h2>
+											<?php if ( $available_seat == 0 ) {
+												do_action( 'mep_show_waitlist_label' );
+											} ?>
+											<h3 class='mep_list_date'> <?php if ( $show_price == 'yes' ) {
+													echo $show_price_label . " " . mep_event_list_price( $values );
+												} ?><!-- <i class="fa fa-calendar"></i> <?php echo date_i18n( 'h:i A', strtotime( $event_meta['mep_event_start_date'][0] ) ); ?> - <?php echo $event_meta['mep_event_end_date'][0]; ?> --></h3>
+										</div>
+										<?php
+										if ( $style == 'grid' ) {
+											?>
+											<div class="mep-event-excerpt">
+												<?php get_the_excerpt( $values ); ?>
+											</div>
+										<?php } ?>
+										<div class="mep-list-footer">
+											<ul>
+												<?php
+	
+												$mep_hide_org_list      = mep_get_option( 'mep_event_hide_organizer_list', 'general_setting_sec' );
+												$mep_hide_location_list = mep_get_option( 'mep_event_hide_location_list', 'general_setting_sec' );
+												$mep_hide_time_list     = mep_get_option( 'mep_event_hide_time_list', 'general_setting_sec' );
+												$mep_hide_end_time_list = mep_get_option( 'mep_event_hide_end_time_list', 'general_setting_sec' );
+	
+	
+												?>
+												<?php if ( $mep_hide_org_list != 'yes' ) { ?>
+													<li>
+														<div class="evl-ico"><i class="fa fa-university"></i></div>
+														<div class="evl-cc">
+															<h5>
+																<?php echo mep_get_option( 'mep_organized_by_text', 'label_setting_sec' ) ? mep_get_option( 'mep_organized_by_text', 'label_setting_sec' ) : _e( 'Organized By:', 'mage-eventpress' ); ?>
+															</h5>
+															<h6><?php if ( $author_terms ) {
+																	echo $author_terms[0]->name;
+																} ?></h6>
+														</div>
+													</li>
+												<?php } ?>
+												<?php if ( $mep_hide_location_list != 'yes' ) { ?>
+													<li>
+														<div class="evl-ico"><i class="fa fa-location-arrow"></i></div>
+														<div class="evl-cc">
+															<h5>
+																<?php echo mep_get_option( 'mep_location_text', 'label_setting_sec' ) ? mep_get_option( 'mep_location_text', 'label_setting_sec' ) : _e( 'Location:', 'mage-eventpress' ); ?>
+	
+															</h5>
+															<h6><?php mep_get_event_city( $values ); ?></h6>
+														</div>
+													</li>
+												<?php } ?>
+												<?php if ( $mep_hide_time_list != 'yes' ) { ?>
+													<li>
+														<div class="evl-ico"><i class="fa fa-calendar"></i></div>
+														<div class="evl-cc">
+															<h5>
+																<?php echo mep_get_option( 'mep_time_text', 'label_setting_sec' ) ? mep_get_option( 'mep_time_text', 'label_setting_sec' ) : _e( 'Time:', 'mage-eventpress' ); ?>
+															</h5>
+															<h6><?php mep_get_only_time( $event_meta['mep_event_start_date'][0] ); ?>
+																<?php if ( $mep_hide_end_time_list != 'yes' ) { ?>
+																	- <?php mep_get_only_time( $event_meta['mep_event_end_date'][0] ); ?>
+																<?php } ?>
+															</h6>
+														</div>
+													</li>
+												<?php } ?>
+											</ul>
+										</div>
+									</a>
+								</div>
+							</div>
+							<?php
+						}
+						wp_reset_postdata();
+					} ?>
+					</div>
+				<?php
+		
+			}
+			public function related_events(){
+				global $woocommerce, $post;
+				$product_ids   = get_post_meta( $post->ID, '_event_list', true );
+				$section_label = get_post_meta( $post->ID, '_related_section_label', true );
+				$column_num    = get_post_meta( $post->ID, '_list_column', true );
+				$smart_theme    = get_post_meta( $post->ID, 'mep_event_template', true );
+				?>
+				<div class="<?php echo $smart_theme=='smart.php'?'mep_smart_theme':''; ?>">
+					<div class="related_events">
+						<h2><?php echo $section_label; ?></h2>
+						<div class="related_items">
+							<?php
+								$event_expire_on 			= mep_get_option( 'mep_event_expire_on_datetime', 'general_setting_sec', 'event_start_datetime');
+								$now                        = current_time('Y-m-d H:i:s');
+								$args_search_qqq = array (
+										'post_type'            => array( 'mep_events' ),
+										'posts_per_page'       => $column_num,
+										'post__in'             => $product_ids,
+										'order'                => 'ASC',
+										'orderby'              => 'meta_value',
+										'meta_key'             => 'event_start_datetime',
+										'meta_query'           => array(
+										array(
+												'key'       => $event_expire_on,
+												'value'     => $now,
+												'compare'   => '>'
+											)
+										)
+
+								);
+								$loop = new WP_Query( $args_search_qqq );
+
+							if ( is_array( $product_ids ) && sizeof($product_ids) > 0 ) {
+								while ($loop->have_posts()) {
+								$loop->the_post(); 
+								$values = get_the_id();
 								$event_meta         = get_post_custom( $values );
 								$tt                 = get_the_terms( $values, 'mep_cat' );
 								$org_class          = mep_get_term_as_class( $values, 'mep_org' );
@@ -208,105 +349,45 @@
 								$cat_class          = mep_get_term_as_class( $values, 'mep_cat' );
 								$available_seat     = mep_get_total_available_seat( $values, $event_meta );
 								$show_price         = mep_get_option( 'mep_event_price_show', 'general_setting_sec', 'yes' );
-								$show_price_label   = mep_get_option( 'event-price-label', 'general_setting_sec', 'Price Starts from:' );
+								$show_price_label   = mep_get_option( 'event-price-label', 'general_setting_sec', 'Price Starts from' );
 								$author_terms       = get_the_terms( $values, 'mep_org' );
 								?>
-								<div class='<?php echo $columnNumber . ' '; ?>mep_event_<?php echo $style; ?>_item mix <?php if ( $tt ) {
-									echo 'mage-' . $org_class;
-								} ?> <?php if ( $torg ) {
-									echo 'mage-' . $cat_class;
-								} ?>'>
-									<div class="mep_list_thumb">
-										<a href="<?php echo get_the_permalink( $values ); ?>"><?php echo get_the_post_thumbnail( $values ); ?></a>
+								<div class="item">
+									<a href="<?php echo get_the_permalink( $values ); ?>">
+										<img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
 										<div class="mep-ev-start-date">
 											<div class="mep-day"><?php echo date_i18n( 'd', strtotime( $event_meta['mep_event_start_date'][0] ) ); ?></div>
 											<div class="mep-month"><?php echo date_i18n( 'M', strtotime( $event_meta['mep_event_start_date'][0] ) ); ?></div>
 										</div>
-									</div>
-									<div class="mep_list_event_details"><a
-												href="<?php echo get_the_permalink( $values ); ?>">
-											<div class="mep-list-header">
-												<h2 class='mep_list_title'><?php echo get_the_title( $values ); ?></h2>
-												<?php if ( $available_seat == 0 ) {
-													do_action( 'mep_show_waitlist_label' );
-												} ?>
-												<h3 class='mep_list_date'> <?php if ( $show_price == 'yes' ) {
-														echo $show_price_label . " " . mep_event_list_price( $values );
-													} ?><!-- <i class="fa fa-calendar"></i> <?php echo date_i18n( 'h:i A', strtotime( $event_meta['mep_event_start_date'][0] ) ); ?> - <?php echo $event_meta['mep_event_end_date'][0]; ?> --></h3>
-											</div>
-											<?php
-											if ( $style == 'grid' ) {
-												?>
-												<div class="mep-event-excerpt">
-													<?php get_the_excerpt( $values ); ?>
-												</div>
-											<?php } ?>
-											<div class="mep-list-footer">
-												<ul>
-													<?php
-		
-													$mep_hide_org_list      = mep_get_option( 'mep_event_hide_organizer_list', 'general_setting_sec' );
-													$mep_hide_location_list = mep_get_option( 'mep_event_hide_location_list', 'general_setting_sec' );
-													$mep_hide_time_list     = mep_get_option( 'mep_event_hide_time_list', 'general_setting_sec' );
-													$mep_hide_end_time_list = mep_get_option( 'mep_event_hide_end_time_list', 'general_setting_sec' );
-		
-		
-													?>
-													<?php if ( $mep_hide_org_list != 'yes' ) { ?>
-														<li>
-															<div class="evl-ico"><i class="fa fa-university"></i></div>
-															<div class="evl-cc">
-																<h5>
-																	<?php echo mep_get_option( 'mep_organized_by_text', 'label_setting_sec' ) ? mep_get_option( 'mep_organized_by_text', 'label_setting_sec' ) : _e( 'Organized By:', 'mage-eventpress' ); ?>
-																</h5>
-																<h6><?php if ( $author_terms ) {
-																		echo $author_terms[0]->name;
-																	} ?></h6>
-															</div>
-														</li>
-													<?php } ?>
-													<?php if ( $mep_hide_location_list != 'yes' ) { ?>
-														<li>
-															<div class="evl-ico"><i class="fa fa-location-arrow"></i></div>
-															<div class="evl-cc">
-																<h5>
-																	<?php echo mep_get_option( 'mep_location_text', 'label_setting_sec' ) ? mep_get_option( 'mep_location_text', 'label_setting_sec' ) : _e( 'Location:', 'mage-eventpress' ); ?>
-		
-																</h5>
-																<h6><?php mep_get_event_city( $values ); ?></h6>
-															</div>
-														</li>
-													<?php } ?>
-													<?php if ( $mep_hide_time_list != 'yes' ) { ?>
-														<li>
-															<div class="evl-ico"><i class="fa fa-calendar"></i></div>
-															<div class="evl-cc">
-																<h5>
-																	<?php echo mep_get_option( 'mep_time_text', 'label_setting_sec' ) ? mep_get_option( 'mep_time_text', 'label_setting_sec' ) : _e( 'Time:', 'mage-eventpress' ); ?>
-																</h5>
-																<h6><?php mep_get_only_time( $event_meta['mep_event_start_date'][0] ); ?>
-																	<?php if ( $mep_hide_end_time_list != 'yes' ) { ?>
-																		- <?php mep_get_only_time( $event_meta['mep_event_end_date'][0] ); ?>
-																	<?php } ?>
-																</h6>
-															</div>
-														</li>
-													<?php } ?>
-												</ul>
-											</div>
-										</a>
+									</a>
+									<div class="item-info">
+										<div class="title">
+											<a href="<?php echo get_the_permalink( $values ); ?>">
+												<h2>
+													<?php echo mb_substr(get_the_title(), 0, 35) . '...'; ?>
+												</h2>
+											</a>
+											<?php echo mb_substr(get_the_title(), 0, 100) . '...'; ?>
+										</div>
+										<div class="price">
+											<p><?php echo $show_price_label   ?></p>
+											<h2 class='mep_list_date'> 
+												<?php if ( $show_price == 'yes' ) {
+														echo mep_event_list_price( $values );
+													} ?>
+											</h2>
+										</div>
 									</div>
 								</div>
 								<?php
-							}
-							wp_reset_postdata();
-						} ?>
+								}
+								wp_reset_postdata();
+							} ?>
+						</div>
 					</div>
 				</div>
 				<?php
-		
 			}
-		
 			public function related_single_products() {
 				global $woocommerce, $post;
 				$product_ids = get_post_meta( $post->ID, '_upsizing_products_ids', true );

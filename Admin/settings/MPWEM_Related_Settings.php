2965134006549c1cb2556e81964bd493a2631a63
Shahadat <raselsha@gmail.com>
2025-01-08 11:03:50 +0600

    fix: event related responsive

--- Admin/settings/MPWEM_Related_Settings.php
+++ Admin/settings/MPWEM_Related_Settings.php
@@ -426,7 +426,25 @@
 								centerMode: true,
 								centerPadding: '10px',
 								slidesToShow: 3,
-								slidesToScroll: 1
+								slidesToScroll: 1,
+								responsive: [
+									{
+									breakpoint: 1024,
+									settings: {
+										slidesToShow:2,
+										slidesToScroll: 2,
+										infinite: true,
+										dots: true
+									}
+									},
+									{
+									breakpoint: 480,
+									settings: {
+										slidesToShow: 1,
+										slidesToScroll: 1
+									}
+									}
+								]
 							});
 						});
 					})(jQuery);

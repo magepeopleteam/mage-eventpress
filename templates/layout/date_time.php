82cc41a0fd496ffc4a5800e80a138c5ced6bf3b4
Shahadat <raselsha@gmail.com>
2025-01-10 08:14:18 +0600

    fix: date time scroll

--- templates/layout/date_time.php
+++ templates/layout/date_time.php
@@ -14,25 +14,27 @@
 		?>
         <div class="date_widgets">
             <i class="far fa-calendar"></i>
-            <div>
-                <h2><?php esc_html_e( 'Date & Time', 'mage-eventpress' ); ?></h2>
-				<?php if ( $date_type == 'no' || $date_type == 'yes' ) {
-				} else {
-					foreach ( $all_dates as $key => $dates ) {
-						$all_times = MPWEM_Functions::get_times( $event_id, $all_dates, $dates );
-						if ( $key > 0 ) { ?>
-                            <div class="_divider_xs"></div>
-						<?php } ?>
-                        <p><?php echo esc_html( MP_Global_Function::date_format( $dates ) ); ?></p>
-						<?php if ( sizeof( $all_times ) > 0 ) {
-							foreach ( $all_times as $time ) {
-								$start_time = array_key_exists( 'start', $time ) ? $time['start']['time'] : '';
-								?>
-                                <p><?php echo esc_html( $time['start']['label']). ' :  '.esc_html( MP_Global_Function::date_format( $start_time, 'time' )); ?></p>
-							<?php }
-						}
-					} ?>
-				<?php } ?>
+            <div class="date_time_box">
+				<h2><?php esc_html_e( 'Date & Time', 'mage-eventpress' ); ?></h2>
+				<div class="date_time">
+					<?php if ( $date_type == 'no' || $date_type == 'yes' ) {
+					} else {
+						foreach ( $all_dates as $key => $dates ) {
+							$all_times = MPWEM_Functions::get_times( $event_id, $all_dates, $dates );
+							if ( $key > 0 ) { ?>
+								<div class="_divider_xs"></div>
+							<?php } ?>
+							<p><?php echo esc_html( MP_Global_Function::date_format( $dates ) ); ?></p>
+							<?php if ( sizeof( $all_times ) > 0 ) {
+								foreach ( $all_times as $time ) {
+									$start_time = array_key_exists( 'start', $time ) ? $time['start']['time'] : '';
+									?>
+									<p><?php echo esc_html( $time['start']['label']). ' :  '.esc_html( MP_Global_Function::date_format( $start_time, 'time' )); ?></p>
+								<?php }
+							}
+						} ?>
+					<?php } ?>
+				</div>
                 <button>
                     <i class="far fa-calendar"></i>
                     <?php _e('Add To Calender','mage-eventpress');?>

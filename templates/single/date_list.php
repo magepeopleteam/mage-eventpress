19ca6668acd55ba15a8957d086e2a16862e8f1b0
Shahadat <raselsha@gmail.com>
2025-01-14 11:24:42 +0600

    fix: event date

--- templates/single/date_list.php
+++ templates/single/date_list.php
@@ -9,12 +9,12 @@
         </span>
         <?php if ($end_date_display_status == 'yes') { ?>
             <span class='mep_date_scdl_end_datetime'>
-                 &nbsp;<span class="mep_date_scdl_separator"> - </span>&nbsp;
+                <span class="mep_date_scdl_separator"> - </span>
                 <?php
                 if ($start_date != $end_date) {
                     echo esc_html(get_mep_datetime($end_datetime, 'date-text'));
                 }
-                echo ' ' . esc_html(get_mep_datetime($end_datetime, 'time'));
+                echo esc_html(get_mep_datetime($end_datetime, 'time'));
                 ?>
             </span>
         <?php } ?>

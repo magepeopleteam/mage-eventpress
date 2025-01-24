bffcbc37fd09ddb828df9d7f3dfb6eda68901c79
sumonazma <engr.sumonazma@gmail.com>
2025-01-24 12:10:57 +0600

    test

--- inc/MPWEM_Woocommerce.php
+++ inc/MPWEM_Woocommerce.php
@@ -53,7 +53,7 @@
 					$cart_item_data['event_cart_display_date']  = $mep_event_start_date[0];
 					do_action( 'mep_event_cart_data_reg' );
 					$cart_item_data['event_id'] = $product_id;
- 
+
 //echo '<pre>';print_r( $user );echo '</pre>';die();
 
 					return apply_filters( 'mep_event_cart_item_data', $cart_item_data, $product_id, $total_price, $user, $ticket_type_arr, $event_extra );

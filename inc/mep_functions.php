a21e5943cbc1291d900bd1a2c636c36861cf60c1
mahin <a.a.mahin@gmail.com>
2025-01-28 10:48:05 +0600

    function updated

--- inc/mep_functions.php
+++ inc/mep_functions.php
@@ -6,8 +6,6 @@
 	define( 'MEP_URL', plugin_dir_url( __DIR__ ) );
 	define( 'MEP_PATH', plugin_dir_path( __DIR__ ) );
 
-
-
 	add_action( 'admin_init',  'mep_flush_rules_event_list_page');
 	function mep_flush_rules_event_list_page() {
 		if ( isset( $_GET['post_type'] ) && sanitize_text_field( wp_unslash($_GET['post_type']) ) == 'mep_events' ) {

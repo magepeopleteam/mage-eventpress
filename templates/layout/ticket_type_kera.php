d9aa10eea4e6f75f28c84730d434809fa959a2f0
Shahadat <raselsha@gmail.com>
2025-04-28 11:57:33 +0600

    style: price table update

--- templates/layout/ticket_type_kera.php
+++ templates/layout/ticket_type_kera.php
@@ -43,47 +43,38 @@
                 <div class="mpTabs">
                     <div class="tabLists mep-kera-theme">
 	                    <?php $tab_count = 0;
-		                    foreach ( $new_tickets as $tickets ) { ?>
-								<div class="pricing-card">
-									<div class="event-header">
-										<div class="event-date">SATURDAY MAY 10</div>
-										<div class="event-logo">stock BAR</div>
-										<div class="event-location">MONTREAL</div>
-									</div>
-									<div class="ticket-type" data-tabs-target="#category_name_<?php echo esc_attr( $tab_count ); ?>">
-										<?php
-											echo esc_html( $tickets['group'] );
-											$tab_count ++;
-                                        ?>
-									</div>
-									<div class="show-time">Show 2:00pm to 4:00pm</div>
-									<div class="food-optional">Food service optional</div>									
-									<div class="food-details">
-										<div class="price">$25 PER PERSON</div>
-										<div class="catered-by">CATERED BY SALOON</div>
-										<div class="buffet-style">Served Buffet Style:</div>
-										<ul class="menu-list">
-											<li>Sushi</li>
-											<li>Beef Tartare</li>
-											<li>Shrimp Cocktail</li>
-											<li>Chicken Skewers</li>
-											<li>Flatbread Pizza</li>
-											<li>Vegetarian Spring Rolls</li>
-											<li>Samosas</li>
-											<li>Mixed Sandwiches</li>
-											<li>Sausage Puff Pastry</li>
-										</ul>
-										<div class="food-service-time">Food Service: 1:00pm to 2:00pm</div>
-									</div>
-									
-									<div class="pricing-footer">
-										<div class="ticket-type-label">GROUP</div>
-										<div class="ticket-qty-column">Ticket Qty:</div>
-										<div class="ticket-price-column">Per Ticket Price:</div>
-									</div>
-									
+						foreach ( $new_tickets as $tickets ) { ?>
+							<div class="mep-pricing-card" data-tabs-target="#category_name_<?php echo esc_attr( $tab_count ); ?>">
+								<div class="mep-pricing-header">
+									<h3 class="event-date">SATURDAY MAY 10</h3>
+									<h2 class="event-logo">stock BAR</h2>
+									<p class="event-location">MONTREAL</p>
 								</div>
-		                    <?php } ?>
+								<div class="mep-ticket-type"> 
+									<h2 class="title"><?php echo esc_html( $tickets['group'] ); $tab_count ++; ?> </h2>
+									<p class="label">Show 2:00pm to 4:00pm</p>
+									<p>Food service optional</p>	
+								</div>
+								<div class="mep-ticket-type"> 
+									<h2>Food Service</h2>
+									<h3>$25 PER PERSON</h3>
+									<h3>CATERED BY SALOON</h3>
+									<p class="label">Served Buffet Style:</p>
+									<ul>
+										<li>Sushi</li>
+										<li>Beef Tartare</li>
+										<li>Shrimp Cocktail</li>
+										<li>Chicken Skewers</li>
+										<li>Flatbread Pizza</li>
+										<li>Vegetarian Spring Rolls</li>
+										<li>Samosas</li>
+										<li>Mixed Sandwiches</li>
+										<li>Sausage Puff Pastry</li>
+									</ul>
+									<p class="label">Food Service: 1:00pm to 2:00pm</p>	
+								</div>
+							</div>
+						<?php } ?>
                     </div>
                     <div class="tabsContent dLayout">
 						<?php $tab_count = 0;

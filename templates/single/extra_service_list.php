ac534e0610f2ebd4c12985c49e8da49156b328c8
Shahadat <raselsha@gmail.com>
2025-04-29 15:05:21 +0600

    refactor: update extra service and summery

--- templates/single/extra_service_list.php
+++ templates/single/extra_service_list.php
@@ -1,5 +1,6 @@
-<h3 class='ex-sec-title mep_extra_service_title'><?php echo esc_html($extra_service_label); ?></h3>
-<table id='mep_event_extra_service_table'>    
+<div id='mep_event_extra_service_table' class="mep-event-ticket-type">
+    <div class="mep-event-ticket-type-header"><?php echo esc_html($extra_service_label); ?></div>
+    
     <?php
     $mep_available_seat         = get_post_meta($post_id, 'mep_available_seat', true) ? get_post_meta($post_id, 'mep_available_seat', true) : 'on';
     foreach ($mep_events_extra_prices as $field) {
@@ -21,46 +22,48 @@
 //        $data_price     = str_replace(wc_get_price_decimal_separator(), '.', $data_price);
         $data_price     = MP_Global_Function::get_wc_raw_price($post_id,$tic_price);
     ?>
-        <tr>
-            <td align="Left"><?php echo esc_html($service_name); ?>
-            <?php if ($mep_available_seat == 'on') { ?>
-                <div class="xtra-item-left"><?php echo esc_html($ext_left); ?>
-                    <?php echo mep_get_option('mep_left_text', 'label_setting_sec', __('Left:', 'mage-eventpress'));  ?>
-                </div>
-                <?php } ?>
-                
-                <input type="hidden" name='mep_event_start_date_es[]' value='<?php echo esc_attr($event_date); ?>'>
-            </td>
-            <td class="mage_text_center">
-                <?php
-                if ($ext_left > 0) {
-                    if ($qty_type == 'dropdown') { ?>
-                        <select name="event_extra_service_qty[]" id="eventpxtp_" class='extra-qty-box'>
-                            <?php for ($i = 0; $i <= $ext_left; $i++) { ?>
-                                <option value="<?php echo esc_attr($i); ?>"><?php echo esc_html($i); ?> <?php echo esc_html($service_name); ?></option>
-                            <?php } ?>
-                        </select>
-                    <?php } else { ?>
-                        <div class="mage_input_group">
-                            <span class="fa fa-minus qty_dec"></span>
-                            <input id="eventpx" size="4" inputmode="numeric" type="text" class='extra-qty-box' name='event_extra_service_qty[]' data-price='<?php echo esc_attr($data_price); ?>' value='0' min="0" max="<?php echo esc_attr($ext_left); ?>">
-                            <span class="fa fa-plus qty_inc"></span>
+        <div class="mep-event-ticket-item">
+            <div class="ticket-info">
+                <div class="ticket-name"><?php echo esc_html($service_name); ?> <span class="badge">Basic</span></div>
+                <!-- // Check if low stock warning was displayed through the hook -->
+                <div class="ticket-remaining remaining-high">
+                    <?php if ($mep_available_seat == 'on') { ?>
+                        <div class="xtra-item-left"><?php echo esc_html($ext_left); ?>
+                            <?php echo mep_get_option('mep_left_text', 'label_setting_sec', __('Remaining', 'mage-eventpress'));  ?>
                         </div>
-                <?php }
-                } else {
-                    echo mep_get_option('mep_not_available_text', 'label_setting_sec', __('Not Available', 'mage-eventpress'));
-                } ?>
-            </td>
-            <td class="mage_text_center"><?php echo wc_price(esc_html(mep_get_price_including_tax($post_id, $service_price)));
-                                            if ($ext_left > 0) { ?>
+                    <?php } ?>
+                    <input type="hidden" name='mep_event_start_date_es[]' value='<?php echo esc_attr($event_date); ?>'>
+                </div>
+            </div>
+            <div class="quantity-control">
+                <?php
+                    if ($ext_left > 0) {
+                        if ($qty_type == 'dropdown') { ?>
+                            <select name="event_extra_service_qty[]" id="eventpxtp_" class='extra-qty-box'>
+                                <?php for ($i = 0; $i <= $ext_left; $i++) { ?>
+                                    <option value="<?php echo esc_attr($i); ?>"><?php echo esc_html($i); ?> <?php echo esc_html($service_name); ?></option>
+                                <?php } ?>
+                            </select>
+                        <?php } else { ?>
+                            <span class="qty-btn qty_dec">-</span>
+                            <input id="eventpx" size="4" inputmode="numeric" type="text" class='extra-qty-box qty-input' name='event_extra_service_qty[]' data-price='<?php echo esc_attr($data_price); ?>' value='0' min="0" max="<?php echo esc_attr($ext_left); ?>">
+                            <span class="qty-btn qty_inc">+</span>
+                    <?php }
+                    } else {
+                        echo mep_get_option('mep_not_available_text', 'label_setting_sec', __('Not Available', 'mage-eventpress'));
+                    } ?>
+	        </div>
+            <div class="ticket-price">
+                <?php echo wc_price(esc_html(mep_get_price_including_tax($post_id, $service_price)));
+                if ($ext_left > 0) { ?>
                     <p style="display: none;" class="price_jq"><?php echo esc_html($tic_price) > 0 ? esc_html($tic_price) : 0;  ?></p>
                     <input type="hidden" name='event_extra_service_name[]' value='<?php echo esc_attr($service_name); ?>'>
                     <input type="hidden" name='event_extra_service_price[]' value='<?php echo esc_attr($service_price); ?>'>
                 <?php } ?>
-            </td>
-        </tr>
+            </div>
+        </div>
     <?php
         $count++;
     }
     ?>
-</table>
+</div>

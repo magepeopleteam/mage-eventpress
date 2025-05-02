f4c3bc5983cb4dad072089c2b16c5ffa3517772c
Shahadat <raselsha@gmail.com>
2025-05-02 10:26:50 +0600

    style: time line update

--- templates/single/daywise_details.php
+++ templates/single/daywise_details.php
@@ -1,22 +1,24 @@
 <div class="mep-day-details-section">
     <h4><?php esc_html_e('Event Timelines', 'mage-eventpress'); ?></h4>   
-    <?php
-    $i = 1;
-    foreach ($mep_event_day as $field):
-        if(!empty($field['mep_day_title'])){
-    ?>
-    <div class="mep-day-details-item">
-        <div class="mep-day-icon"><?php echo $i; ?></div>
-        <div class="mep-day-content">
-            <div class="mep-day-title"><?php echo esc_html($field['mep_day_title']); ?></div>
-            <div class="mep-day-details">
-                <?php echo mep_esc_html(html_entity_decode(nl2br($field['mep_day_content']))); ?>
+    <div class="timeline">
+        <?php
+        $i = 1;
+        foreach ($mep_event_day as $field):
+            if(!empty($field['mep_day_title'])){
+        ?>
+        <div class="mep-day-details-item">
+            <div class="mep-day-icon"><?php echo $i; ?></div>
+            <div class="mep-day-content">
+                <div class="mep-day-title"><?php echo esc_html($field['mep_day_title']); ?><span class="mep-day-time">8:00 AM - 9:00 AM</span></div>
+                <div class="mep-day-details">
+                    <?php echo mep_esc_html(html_entity_decode(nl2br($field['mep_day_content']))); ?>
+                </div>
             </div>
-        </div>
-    </div>    
-    <?php
-    $i++;
-    }
-    endforeach;
-    ?>
+        </div>    
+        <?php
+        $i++;
+        }
+        endforeach;
+        ?>
+    </div>
 </div>

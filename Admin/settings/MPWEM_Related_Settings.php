7632d055bb88492ae2f22dd5a1748a5e13c63f83
Shahadat <raselsha@gmail.com>
2025-08-26 11:24:02 +0600

    fix: related event settings

--- Admin/settings/MPWEM_Related_Settings.php
+++ Admin/settings/MPWEM_Related_Settings.php
@@ -410,28 +410,30 @@
 								prevArrow:'.mep-ev-prev',
 								nextArrow:'.mep-ev-next',
 								infinite: true,
-								centerMode: true,
-								autoplay: false,
+								centerMode: false, // Make sure centerMode is false
+								autoplay: true,
 								autoplaySpeed: 2000,
-								centerPadding: '10px',
+								centerPadding: '0px',
 								slidesToShow: 3,
 								slidesToScroll: 1,
 								responsive: [
 									{
-									breakpoint: 1024,
-									settings: {
-										slidesToShow:2,
-										slidesToScroll: 2,
-										infinite: true,
-										dots: true
-									}
+										breakpoint: 1024,
+										settings: {
+											slidesToShow:2,
+											slidesToScroll: 2,
+											infinite: true,
+											dots: true,
+											centerMode: false // Ensure left alignment on responsive
+										}
 									},
 									{
-									breakpoint: 480,
-									settings: {
-										slidesToShow: 1,
-										slidesToScroll: 1
-									}
+										breakpoint: 480,
+										settings: {
+											slidesToShow: 1,
+											slidesToScroll: 1,
+											centerMode: false // Ensure left alignment on mobile
+										}
 									}
 								]
 							});
